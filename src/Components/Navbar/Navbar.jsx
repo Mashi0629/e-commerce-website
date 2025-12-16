18a225adffc2e97edfb80b76cc7a7539b9fa3644
Mashi0629 <poboshiniomashi@gmail.com>
2025-12-16 20:05:49 +0530

    update navbar.js

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,8 +7,23 @@ import cart_icon from '../Assets/cart.png'
 const Navbar = () => {
     return (
         <div className="navbar">
+           <div className="nav-logo">
+                <img src={logo} alt="Logo" className="logo-image" />
+                <p>Shopper</p>
+              </div>
+              <ul className="nav-menu">
+                <li>Shop</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>Kids</li>
 
+              </ul>
+                <div className="nav-login-cart">
+                    <button>Login</button>
+                    <img src={cart_icon} alt="Cart" />
+                </div>
         </div>
+        
     );
 };
 
