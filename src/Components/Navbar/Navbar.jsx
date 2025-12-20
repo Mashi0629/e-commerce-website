a2ea061bf40b120cf482497a2178de30419868d3
Mashi0629 <poboshiniomashi@gmail.com>
2025-12-20 21:05:19 +0530

    nav.css

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -21,6 +21,8 @@ const Navbar = () => {
                 <div className="nav-login-cart">
                     <button>Login</button>
                     <img src={cart_icon} alt="Cart" />
+                    <div className="nav-cart-count">0</div>
+                
                 </div>
         </div>
         
