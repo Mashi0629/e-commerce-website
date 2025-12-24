d454e4b57aa8e912c35600ab6a8e0c442b09c458
Mashi0629 <poboshiniomashi@gmail.com>
2025-12-24 20:15:35 +0530

    update navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,10 +16,10 @@ const Navbar = () => {
                 <p>Shopper</p>
               </div>
               <ul className="nav-menu">
-                <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"? <hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("men")}}>Men{menu==="men"? <hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("women")}}>Women{menu==="women"? <hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"? <hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("shop")}}><Link to= '/'>Shop</Link>{menu==="shop"? <hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("men")}}><Link to= '/men'>Men</Link>{menu==="men"? <hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("women")}}><Link to= '/women'>Women</Link>{menu==="women"? <hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("kids")}}><Link to= '/kids'>Kids</Link> {menu==="kids"? <hr/>:<></>}</li>
 
               </ul>
                 <div className="nav-login-cart">
