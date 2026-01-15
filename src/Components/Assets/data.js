734efa98867eabdd4f628543aa6f5a7405d7cd12
Mashi0629 <poboshiniomashi@gmail.com>
2026-01-15 20:54:34 +0530

    insert products

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,9 @@
+
+
+
+let data_product = [
+    {
+        id: 1,
+        img: "https://m.media-amazon.com/images/I/61jLiCov4bL._AC_UY550_.jpg",
+        name: "boAt Rockerz 255 Pro+",
+        new_price: "₹1,999.00",
