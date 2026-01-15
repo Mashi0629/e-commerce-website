ce6403f6f3a7120f720c8668e06ace48899330e1
Mashi0629 <poboshiniomashi@gmail.com>
2026-01-15 22:17:37 +0530

    update items

--- src/Components/Assets/data.js
+++ src/Components/Assets/data.js
@@ -1,9 +1,38 @@
-
+import p1_img from "../../Assets/p1_img.jpg";
+import p2_img from "../../Assets/p2_img.jpg";
+import p3_img from "../../Assets/p3_img.jpg";
+import p4_img from "../../Assets/p4_img.jpg";
 
 
 let data_product = [
     {
         id: 1,
-        img: "https://m.media-amazon.com/images/I/61jLiCov4bL._AC_UY550_.jpg",
-        name: "boAt Rockerz 255 Pro+",
-        new_price: "₹1,999.00",
+        img: p1_img
+        name: "office blouse",
+        new_price: "$39.00",
+        old_price: "$49.00"
+    },
+    {
+        id: 2,
+        img: p2_img,
+        name: "office blouse",
+        new_price: "$39.00",
+        old_price: "$49.00"
+    }
+    ,
+    {
+        id: 3,
+        img: p3_img,
+        name: "office blouse",
+        new_price: "$39.00",
+        old_price: "$49.00"
+    },
+    {
+        id: 4,
+        img: p4_img,
+        name: "office blouse",
+        new_price: "$39.00",
+        old_price: "$49.00"
+    }   
+];
+export default data_product;
--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Popular.css'
-
+import data_product from '../Assets/data'
 const Popular = () => {
     return (
         <div className='popular'>
+            <h1>Popular In Women </h1>
+            <hr />
+            <div className="popular-item">
+
+            </div>
+
 
         </div>
     )
