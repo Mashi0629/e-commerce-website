97db2cc75f801eeb685a7fc06d0adc973d784d17
Mashi0629 <poboshiniomashi@gmail.com>
2026-01-18 12:04:53 +0530

    solve error in items

--- src/Components/Assets/data.js
+++ src/Components/Assets/data.js
@@ -1,13 +1,14 @@
-import p1_img from "../../Assets/p1_img.jpg";
-import p2_img from "../../Assets/p2_img.jpg";
-import p3_img from "../../Assets/p3_img.jpg";
-import p4_img from "../../Assets/p4_img.jpg";
+import p1_img from "./p1_img.jpg";
+import p2_img from "./p2_img.jpg";
+import p3_img from "./p3_img.jpg";
+import p4_img from "./p4_img.jpg";
+
 
 
 let data_product = [
     {
         id: 1,
-        img: p1_img
+        img: p1_img,
         name: "office blouse",
         new_price: "$39.00",
         old_price: "$49.00"
--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import './Popular.css'
-import data_product from '../Assets/data'
+import React from 'react';
+import './Popular.css';
+import data_product from '../Assets/data';
+import Item from '../Item/Item';
+
 const Popular = () => {
     return (
         <div className='popular'>
@@ -8,7 +10,16 @@ const Popular = () => {
             <hr />
             <div className="popular-item">
                 {data_product.map((item) => {
-                    return <Item key={item.id} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                    return 
+                    <Item 
+                    key={item.id} 
+                    id={item.id} 
+                    name={item.name} 
+                    image={item.img}
+                    new_price={item.new_price} 
+                    old_price={item.old_price}
+                    />
+                
                 })}
 
             </div>
