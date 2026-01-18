59112c9e6bbce64b9b19a436d4a2e4ce53625683
Mashi0629 <poboshiniomashi@gmail.com>
2026-01-18 11:22:43 +0530

    not displaying items in homepage

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -7,6 +7,9 @@ const Popular = () => {
             <h1>Popular In Women </h1>
             <hr />
             <div className="popular-item">
+                {data_product.map((item) => {
+                    return <Item key={item.id} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                })}
 
             </div>
 
