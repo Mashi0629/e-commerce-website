17684aa24335f74b63253c32384b746a280d976b
Mashi0629 <poboshiniomashi@gmail.com>
2026-01-26 19:26:46 +0530

    Update Popular.jsx
    fix the error of not displaying img

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -4,28 +4,24 @@ import data_product from '../Assets/data';
 import Item from '../Item/Item';
 
 const Popular = () => {
-    return (
-        <div className='popular'>
-            <h1>Popular In Women </h1>
-            <hr />
-            <div className="popular-item">
-                {data_product.map((item) => {
-                    return 
-                    <Item 
-                    key={item.id} 
-                    id={item.id} 
-                    name={item.name} 
-                    image={item.img}
-                    new_price={item.new_price} 
-                    old_price={item.old_price}
-                    />
-                
-                })}
+  return (
+    <div className='popular'>
+      <h1>Popular In Women</h1>
+      <hr />
+      <div className="popular-item">
+        {data_product.map((item) => (
+          <Item
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            img={item.img}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-            </div>
-
-
-        </div>
-    )
-}
 export default Popular;
